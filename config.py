68d656c8bf128ee3a09f199ffb7f79e1f2c3f193
Rooty0p69 <tanmoylegal111@gmail.com>
2024-09-07 19:56:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7457723855:AAHU4qd08bAOA0t51IYLQO8wO5tZU4H0xcU")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@Supplier2334")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1136925319")
 
 
 
